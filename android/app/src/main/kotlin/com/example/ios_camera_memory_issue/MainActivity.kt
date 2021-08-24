9ccd45dc3ac9a5ce42f645b47d23ab85e877d325
timur.khismatullin <timur.khismatullin@umbrellait.com>
2021-08-24 17:29:20 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ios_camera_memory_issue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ios_camera_memory_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                    builder: (BuildContext context) => CameraScreen()));
+          },
+          color: Colors.blueAccent,
+          child: Text('Open Camera'),
+        ),
+      ),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? controller;
+  Completer<bool> completer = Completer<bool>();
+
+  @override
+  void initState() {
+    initCamera();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<bool>(
+        future: completer.future,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Center(child: CameraPreview(controller!));
+          } else
+            return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Future initCamera() async {
+    final List<CameraDescription> cameras = await availableCameras();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    await controller!.initialize();
+    completer.complete(true);
+  }
+}
